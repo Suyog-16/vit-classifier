e9113463ddf1334cd3ab783f240e7752d2ee50b7
Suyog Ghimire <ghimiresuyog89@gmail.com>
2025-05-29 12:15:03 +0545

    modified for config.yml

--- models/vit.py
+++ models/vit.py
@@ -8,6 +8,15 @@ from tqdm import tqdm
 import sys
 import os
 from torch.utils.tensorboard import SummaryWriter
+import yaml
+with open("config.yml",'r') as f:
+    config = yaml.safe_load(f)
+
+train_config = config["train"]
+model_config = config["model"]
+dataset_config = config["dataset"]
+device = config["device"]
+
 
 writer = SummaryWriter(log_dir= "logs/vit_experiment")
 
@@ -15,36 +24,36 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.train_helpers import train
 
 
-transform = transforms.Compose([transforms.Resize((224,224)),
+transform = transforms.Compose([transforms.Resize((dataset_config['image_size'],dataset_config['image_size'])),
                                transforms.ToTensor(),
-                               transforms.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5))
+                               transforms.Normalize(mean=dataset_config['normalize_mean'], std=dataset_config['normalize_std'])
                                ])
 
-train_data = datasets.CIFAR10(root="data",
+train_data = datasets.CIFAR10(root=dataset_config['data_dir'],
                                train=True,
                                download = True,
                                transform = transform )
-test_data = datasets.CIFAR10(root= 'data',
+test_data = datasets.CIFAR10(root= dataset_config['data_dir'],
                               train = False,
                               download = True,
                               transform = transform)
 
 ## DataLoader
-train_loader = DataLoader(train_data,batch_size=32,shuffle=True)
-test_loader = DataLoader(test_data,batch_size=32,shuffle=False)
+train_loader = DataLoader(train_data,batch_size=train_config['batch_size'],shuffle=True)
+test_loader = DataLoader(test_data,batch_size=train_config['batch_size'],shuffle=False)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(device)
 
-model_vit = timm.create_model('mobilevit_xxs',pretrained = True,num_classes = 10)
+model_vit = timm.create_model(model_config['name'],pretrained = model_config["pretrained"],num_classes = 10)
 model_vit.to(device)
 
 dummy_input = torch.randn(1,3,224,224).to(device)
 writer.add_graph(model_vit,dummy_input) # for visualizing model
 
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model_vit.parameters(),lr=0.001)
-results = train(model_vit,train_loader,test_loader,optimizer,criterion,writer = writer)
+optimizer = torch.optim.Adam(model_vit.parameters(),lr=train_config["learning_rate"])
+results = train(model_vit,train_loader,test_loader,optimizer,criterion,writer = writer,epochs=train_config['epochs'])
 
 torch.save(model_vit.state_dict(),"checkpoints/model_vit.pth")
 
